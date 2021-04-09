aa8596388274a2b247c884c67b3787803c1aff97
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-04-09 13:50:42 +0300

    Car list fetched from backend and delete function added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Carlist from "./components/Carlist";
 import "./App.css";
+import Carlist from "./components/Carlist";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">CarShop</Typography>
+        </Toolbar>
+      </AppBar>
       <Carlist />
     </div>
   );
--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,45 +1,67 @@
-import React, { useState, useEffect } from "react";
-import ReactTable from "react-table";
-export default function Carlist() {
+import React, { useEffect, useState } from "react";
+
+import { AgGridReact } from "ag-grid-react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+
+function Carlist() {
   const [cars, setCars] = useState([]);
 
-  useEffect(() => fetchData(), []);
+  useEffect(() => {
+    fetchCars();
+  }, []);
 
-  const fetchData = () => {
-    fetch("http://carrestapi.herokuapp.com/cars")
-      .then((response) => response.json())
-      .then((data) => setCars(data._embedded.cars));
+  const deleteCar = (url) => {
+    window.confirm("Are you sure?");
+    fetch(url, { method: "DELETE" }).then((response) => {
+      if (response.ok) fetchCars();
+      else alert("Something terrible happened");
+    });
   };
+
+  const fetchCars = () => {
+    fetch("https://carstockrest.herokuapp.com/cars")
+      .then((response) => response.json())
+      .then((data) => setCars(data._embedded.cars))
+      .catch((err) => console.error(err));
+  };
+
   const columns = [
+    { field: "brand", sortable: true, filter: true },
+    { field: "color", sortable: true, filter: true },
+    { field: "price", sortable: true, filter: true, width: 100 },
+    { field: "model", sortable: true, filter: true },
+    { field: "year", sortable: true, filter: true, width: 100 },
+    { field: "fuel", sortable: true, filter: true },
     {
-      Header: "Brand",
-      accessor: "brand",
-    },
-    {
-      Header: "Model",
-      accessor: "model",
-    },
-    {
-      Header: "Color",
-      accessor: "color",
-    },
-    {
-      Header: "Fuel",
-      accessor: "fuel",
-    },
-    {
-      Header: "Price",
-      accessor: "price",
-    },
-    {
-      Header: "Year",
-      accessor: "year",
+      headerName: "",
+      width: 100,
+      field: "_links.self.href",
+      cellRendererFramework: (params) => (
+        <IconButton color="secondary" onClick={() => deleteCar(params.value)}>
+          <DeleteIcon />
+        </IconButton>
+      ),
     },
   ];
 
   return (
-    <div>
-      <ReactTable data={cars} columns={columns} />
+    <div
+      className="ag-theme-material"
+      style={{ height: 600, width: "90%", margin: "auto" }}
+    >
+      <AgGridReact
+        rowData={cars}
+        columnDefs={columns}
+        pagination={true}
+        paginationPageSize={10}
+        suppressCellSelection={true}
+      />
     </div>
   );
 }
+
+export default Carlist;
